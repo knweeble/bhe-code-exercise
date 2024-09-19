3324f98b2196985553b41540d14636e8741f155c
Katie Strader <knweber.1217@gmail.com>
2024-09-19 11:31:39 -0700

    C# code passes tests. NthPrime method returns the nth prime number

--- csharp/Sieve/Sieve.cs
+++ csharp/Sieve/Sieve.cs
@@ -7,8 +7,37 @@ public interface ISieve
 
 public class SieveImplementation : ISieve
 {
+    private readonly List<long> primes = new();
+    static readonly int MAX_SIZE = 200000000;
+    public SieveImplementation()
+    {
+        List<bool> isPrime = Enumerable.Repeat(true, MAX_SIZE).ToList();
+
+        int i = 2;
+
+        while (i * i < MAX_SIZE)
+        {
+            if (isPrime[i])
+            {
+                int j = i * i;
+                while (j < MAX_SIZE)
+                {
+                    isPrime[j] = false;
+                    j += i;
+                }
+            }
+            i++;
+        }
+        for (int p = 2; p < MAX_SIZE; p++)
+        {
+            if (isPrime[p])
+            {
+                primes.Add(p);
+            }
+        }
+    }
     public long NthPrime(long n)
     {
-        throw new NotImplementedException("unimplemented");
+        return primes[(int)n];
     }
 }
